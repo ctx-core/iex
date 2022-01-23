ac3fbeca8a58a490018389100d884779c2e4763e
Brian Takita <brian.takita@gmail.com>
2022-01-23 00:01:22 -0600

    update dependencies

--- src/iex_fetch.ts
+++ src/iex_fetch.ts
@@ -3,7 +3,7 @@ import { iex_fetch_arg_a_ } from './iex_fetch_arg_a_.js'
 import type { iex_fetch_opts_I } from './iex_fetch_opts_I.js'
 export async function iex_fetch(path:string, in_opts:iex_fetch_opts_I = {}):Promise<Response> {
 	const [url, opts] = iex_fetch_arg_a_(path, in_opts)
-	return fetch(url, opts) as Promise<Response>
+	return fetch(url, opts)
 }
 export {
 	iex_fetch as fetch__iex
